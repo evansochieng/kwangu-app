e31302f4bb9f99cd57b0462859189c7f9aa64c5a
nthigadenis <denno@DESKTOP-TK85V7G.localdomain>
2022-12-06 17:26:36 +0300

    login

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,16 +1,26 @@
 import Login from "./Login";
 import  Register  from "./Register";
+import Navbar from "./Navbar";
+import {Routes,Route,BrowserRouter} from "react-router-dom"
+import Home from "./Home";
 
 function App() {
 
     return  (
     <div>
-     <Login/>
-     <Register/>
+      <Navbar/>
+   <BrowserRouter>
+   <Routes>
+   <Route path="/" element={<Home/>}/>
+       <Route path="/login" element={<Login/>}/>
+      <Route path="/register" element={<Register/>}/>
+
+   </Routes>
+   </BrowserRouter> 
    
     </div>
     )
     };
  export default App;
   
-    
+  
--- client/src/components/Register.js
+++ client/src/components/Register.js
@@ -1,123 +1,67 @@
-import { useState } from 'react';
+import React from 'react'
 
- function Register() {
+function Register() {
+  return (
+	<div className="forms-page">
+      <form className="forms">
+        <br></br>
+        <h2 className="title">Register</h2>
+        <br></br>
+        <br></br>
+        <div className="form">
+          <div className="inputfield">
+            <input
+              type="text"
+              className="form-group form-control"
+              placeholder="Name"
+            />
+          </div>
+          
+          <div className="inputfield">
+            <input
+              type="text"
+              className="form-group form-control"
+              placeholder="Contacts"
+            />
+          </div>
+         
+          <div className="inputfield">
+            <input
+              type="password"
+              className="form-group form-control"
+              placeholder="Password"
+            />
+          </div>
+          <div className="inputfield">
+            <input
+              type="password"
+              className="form-group form-control"
+              placeholder="Confirm Password"
+            />
+          </div>
+          
+          <br></br>
+          <div className="inputfield terms">
+            <label className="check">
+              <input type="checkbox" />
+              <span className="checkmark"></span>
+            </label>
+            <p>Agreed to terms and conditions</p>
+          </div>
+          <button
+            className="btn btn-primary"
+            type="submit">
+           
+              Register
+            
+          </button>
+        </div>
+      </form>
+    </div>
+  );
+	
 
-// States for registration
-const [name, setName] = useState('');
-const [email, setEmail] = useState('');
-const [contact, setContact] = useState('');
-const [gender, setGender] = useState('');
-const [password, setPassword] = useState('');
-
-// States for checking the errors
-const [submitted, setSubmitted] = useState(false);
-const [error, setError] = useState(false);
-
-// Handling the name change
-const handleName = (e) => {
-	setName(e.target.value);
-	setSubmitted(false);
-};
-
-// Handling the email change
-const handleEmail = (e) => {
-	setEmail(e.target.value);
-	setSubmitted(false);
-};
-
-//handling the contact change
-const handleContact = (e) => {
-	setContact(e.target.value);
-	setSubmitted(false);
-};
-// handling the contact change
-const handleGender= (e) => {
-	setGender(e.target.value);
-	setSubmitted(false);
-};
-// Handling the password change
-const handlePassword = (e) => {
-	setPassword(e.target.value);
-	setSubmitted(false);
-};
-
-// Handling the form submission
-const handleSubmit = (e) => {
-	e.preventDefault();
-	if (name === '' || email === '' ||contact=== ''|| gender===''|| password === '') {
-	setError(true);
-	} else {
-	setSubmitted(true);
-	setError(false);
-	}
-};
-
-// Showing success message
-const successMessage = () => {
-	return (
-	<div
-		className="success"
-		style={{
-		display: submitted ? '' : 'none',
-		}}>
-		<h1>User {name} successfully registered!!</h1>
-	</div>
-	);
-};
-
-// Showing error message if error is true
-const errorMessage = () => {
-	return (
-	<div
-		className="error"
-		style={{
-		display: error ? '' : 'none',
-		}}>
-		<h1>Please enter all the fields</h1>
-	</div>
-	);
-};
-
-return (
-	<div className="form">
-	<div>
-		<h1>User Registration</h1>
-	</div>
-
-	{/* Calling to the methods */}
-	<div className="messages">
-		{errorMessage()}
-		{successMessage()}
-	</div>
-
-	<form>
-		{/* Labels and inputs for form data */}
-		<label className="label">Name</label>
-		<input onChange={handleName} className="input"
-		value={name} type="text" />
-
-		<label className="label">Email</label>
-		<input onChange={handleEmail} className="input"
-		value={email} type="email" />
-
-       <label className="label">Contact</label>
-		<input onChange={handleContact} className="input"
-		value={contact} type="contact" />
-
-       <label className="label">Email</label>
-		<input onChange={handleGender} className="input"
-		value={gender} type="gender" />
-
-
-		<label className="label">Password</label>
-		<input onChange={handlePassword} className="input"
-		value={password} type="password" />
-
-		<button onClick={handleSubmit} className="btn" type="submit">
-		Submit
-		</button>
-	</form>
-	</div>
-);
 }
-export default Register;
+
+export default Register
+
